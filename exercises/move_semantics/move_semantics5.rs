8dcf2405b02b8285a2007c64a9b0bffcc685fa09
yangfengshan2 <yangfengshan2@jd.com>
2023-10-10 21:10:09 +0800

    l

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,14 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
+    //https://github.com/rust-lang/rustlings/issues/809
+    // https://doc.rust-lang.org/nomicon/lifetimes.html#the-area-covered-by-a-lifetime
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut *y;
     *z += 1000;
     assert_eq!(x, 1200);
 }
