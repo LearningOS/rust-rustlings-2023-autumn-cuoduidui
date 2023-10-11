4cd265bb4a5a7c7c87d28bf27a8bb92462b2d7df
yangfengshan2 <yangfengshan2@jd.com>
2023-10-11 11:27:37 +0800

    练习

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,10 +6,15 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-
+// Rust中使用&符号表示引用，也叫引用操作符。其使用场景是只使用类型的值但不获取其所有权，同时Rust的引用规则为：
+// 1在作用域中的数据有且只能有一个可变引用；
+// 可以有多个不可变引用；
+// 不能同时拥有不可变引用和可变引用。
+// 引用必须总是有效的
 fn main() {
     //https://github.com/rust-lang/rustlings/issues/809
     // https://doc.rust-lang.org/nomicon/lifetimes.html#the-area-covered-by-a-lifetime
+    // https://blog.51cto.com/u_14834727/3031717
     let mut x = 100;
     let y = &mut x;
     *y += 100;
