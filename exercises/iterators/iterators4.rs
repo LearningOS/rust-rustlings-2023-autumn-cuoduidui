4eecd5a19f4cbf73637cec75ff919d8798a1f2a3
yangfengshan2 <yangfengshan2@jd.com>
2023-10-12 21:20:08 +0800

    练习

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +15,9 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+//     通过range产生连续的数值
+// 通过fold函数进行累积
+    (1u64..num+1).into_iter().fold(1u64, |sum, acm| sum * acm)
 }
 
 #[cfg(test)]
